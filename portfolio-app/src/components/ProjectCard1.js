178cb8f2aa0254bfa3e40da9bff83b27b6f12276
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-07-16 16:31:48 +0300

    project responsivness

--- portfolio-app/src/components/ProjectCard1.js
+++ portfolio-app/src/components/ProjectCard1.js
@@ -6,27 +6,27 @@ const ProjectCard1 = () => {
   return (
     <Container className="project-card text-white text-opacity-75">
       <Row>
-        <Col lg={4} xs={7} className="d-flex flex-column justify-content-around">
+        <Col lg={4} xs={0} className="d-flex flex-column justify-content-around d-none d-lg-block">
           <div><img src="img/bottle-buddy.png"
             alt="bootle-buddy-project png"
             width="100%"
-            s/>
+          />
           </div>
-          <div className="">
-          <span className="skill">
+          <div className="p-2">
+            <span className="skill ms-3">
               <a className="link-button text-decoration-none"
                 target="_blank"
                 rel="noreferrer"
                 href="https://github.com/Chekinm/bottle-buddy">
-                Source code
+                GitHub
               </a>
-            </span> 
+            </span>
             <span className="skill ms-3">
               <a className="link-button text-decoration-none"
                 target="_blank"
                 rel="noreferrer"
                 href="https://bottle-buddy-front.onrender.com/">
-                Live demo
+                Demo
               </a>
             </span>
           </div>
@@ -35,20 +35,39 @@ const ProjectCard1 = () => {
         <Col xs={12} lg={8}>
 
           <h2> <a className="link-highligth text-decoration-none"
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://bottle-buddy-front.onrender.com/">Bottle-buddy project</a></h2>
+            target="_blank"
+            rel="noreferrer"
+            href="https://bottle-buddy-front.onrender.com/">Bottle-buddy project</a></h2>
           <p>
-          The project is a social network connecting people who care about recycling bottles. Users create orders for recycling their bottles, visible to others. Collectors browse and select orders, which the creator can close. The collector picks up the bottles and delivers them to a recycling center, receiving compensation.</p>
+            The project is a social network connecting people who care about recycling bottles. Users create orders for recycling their bottles, visible to others. Collectors browse and select orders, which the creator can close. The collector picks up the bottles and delivers them to a recycling center, receiving compensation.</p>
           <p> The project uses Django backend with Django Rest Framework and React frontend for user authentication, order management, and communication. It aims to streamline recycling by connecting users for convenient and efficient order creation and management.</p>
 
         </Col>
-        {/* <Col xs={9} className="d-xs-block">
+        <Col xs={12} lg={0} className="d-bock d-sm-none">
           <img src="img/bottle-buddy.png"
             alt="bootle-buddy-project png"
             width="100%"
           />
-        </Col> */}
+
+          <div className="p-2">
+            <span className="skill ms-3">
+              <a className="link-button text-decoration-none"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Chekinm/bottle-buddy">
+                GitHub
+              </a>
+            </span>
+            <span className="skill ms-3">
+              <a className="link-button text-decoration-none"
+                target="_blank"
+                rel="noreferrer"
+                href="https://bottle-buddy-front.onrender.com/">
+                Demo
+              </a>
+            </span>
+          </div>
+        </Col>
       </Row>
     </Container>
   )
