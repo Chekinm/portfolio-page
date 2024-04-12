17c0377f002634409c970f95c70b87ea3e08cb60
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-04-12 17:02:45 +0300

    vercel analitics

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
+
+inject(); // Inject vercel the tracking script
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
