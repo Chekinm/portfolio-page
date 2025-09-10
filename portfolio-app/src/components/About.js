ff33106aad1298e3450bec426d30382f07bfcb28
Chekinm <120395978+Chekinm@users.noreply.github.com>
2025-09-10 17:07:50 +0300

    some  updates

--- portfolio-app/src/components/About.js
+++ portfolio-app/src/components/About.js
@@ -6,35 +6,37 @@ const About = () => {
     <article className="fs-5 text-white text-opacity-75" id="about">
       <h4>Hello and welcome to my page</h4>
       <br />
-      <p>My name in Mikhail. After making an aliya in 2022, I decided to change my career path to something that has always interested me. 
-      However, I always got caught up with other "more important" things whenever I thought about pursuing it. </p>
-      <p>Dramatic life change has actually made me more focused on my true passions.  
-      At the beginning of this year, I took the plunge and decided to become a programmer. 
-      I taught myself Python for three months,  I enrolled in the 
-        <a className="link-highligth text-decoration-none"
-          target="_blank"
-          rel="noreferrer"
-          href="https://developers.institute/en/"> Developers Institute Bootcamp, </a>
-         which I completed successfully in July 2023.
+      <p>My name in Mikhail. You reach my personal website. Nice to meet you.
       </p>
-      <p> Currently, I working as a frontend developer in 
+      
+      <p> Currently, I'm working as a frontend developer in 
         <a className="link-highligth text-decoration-none"
           target="_blank"
           rel="noreferrer"
           href="https://gaintrade.ai"> GainTrade Inc</a>. 
-          We build an app for stock exchange traders to manage arbitrage trading strategies. </p> 
-      <p>Below, you will find some of my personal projects 
+          We build an app for stock exchange traders to manage arbitrage trading strategies. We are in Vue.js universe.</p>
+      
+      <p> I'm also volunteering in   
+        <a className="link-highligth text-decoration-none"
+          target="_blank"
+          rel="noreferrer"
+          href="https://symbolstory.app/"> SymbolStory </a> 
+          as a fullstack developer. This is a project for children with special needs. We're building an app which creates therapeutic social stories using AI. 
+          We use React and Node.js, both with TypeScript, MongoDB, Azure Claud, Claude and different textToImage LLM. </p> 
+
+      <p>Below, you will find some of my personal projects
+         
           <Link to="/#projects"
           className="link-highligth text-decoration-none"
-          > projects </Link> I have recently completed. </p>
+          > projects </Link> I made during my education. They were created quite a while ago and might need some polish, so don't judge too harshly. Anyway there are still some funny staff, so I decided to leave it here. </p>
 
-      <p>If you're interested, please feel free to review my 
-        <a href="/MikhailChekin_CV_FullStack_2024_august.pdf"
+      <p>Here you can find my 
+        <a href="/Mikhail_Chekin_CV_Fullstack_Developer_July_2025.pdf"
           target="_blank"
           rel="noreferrer"
           className="link-highligth text-decoration-none"
-        > CV.</a></p>
-      <p>I am always open to communication, so please don't hesitate to  <Link to="/#contacts"
+        > CV</a>
+        . I am always open to talk, so please don't hesitate to  <Link to="/#contacts"
         className="link-highligth text-decoration-none"
       > reach out to me </Link>  with any questions you may have.</p>
 
--- portfolio-app/src/components/Contacts.js
+++ portfolio-app/src/components/Contacts.js
@@ -5,10 +5,10 @@ const Contacts = () => {
     <article className="fs-5 text-white text-opacity-75" id="contacts">
       <h2>Contacts</h2>
       <br />
-        <p>I live in Rishon LeZion.
-        I am considering jobs locations in Gushdan, Jerusalem, Ashkelon and Ashdod.
+        <p>I live Israel.
+        I am considering jobs located in Gushdan, Jerusalem or Ashdod.
         You can always reach me by phone.</p>
-        <p> You can find a number in <a href="MikhailChekin_CV_FullStack_2024_august.pdf"
+        <p> You can find a number in <a href="Mikhail_Chekin_CV_Fullstack_Developer_July_2025.pdf"
           target="_blank"
           rel="noreferrer"
           className="link-highligth text-decoration-none"
